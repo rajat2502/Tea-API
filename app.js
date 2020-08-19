1969cb3ac3e91724e961a9d40771ae2fea5e647b
Rajat Verma <rajatverma5885045@gmail.com>
2020-08-20 01:40:43 +0530

    add deps for security

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const helmet = require('helmet');
+const compression = require('compression');
 const teaRouter = require('./routes/tea');
 
 const app = express();
 
 // Middlewares
+
+// Helmet - for app security
+app.use(helmet());
+
+// Compression - compress response, for lesser request time
+app.use(compression());
 
 // Make uploads folder static
 app.use('/uploads', express.static('./uploads'));
